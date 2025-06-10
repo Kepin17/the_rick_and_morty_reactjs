23f6e8322dea337aec1c85fe3938bca38b2e1ddd
kepin17 <servicenya4@gmail.com>
2025-06-10 23:28:04 +0700

    refactor: refactor card to component and add get api https://rickandmortyapi.com/api/character with axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
+import axios from "axios";
 import "./App.css";
 import Button from "./components/elements/Button";
+import { useEffect, useState } from "react";
+import CardFragments from "./components/fragments/CardFragment";
 
 function App() {
+  const [datas, setDatas] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://rickandmortyapi.com/api/character")
+      .then((res) => {
+        setDatas(res.data.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <>
       <div className="card-wrapper w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
-        <div className="card h-auto border-2 border-gray-300 rounded-lg shadow-lg">
-          <div className="header-card w-full">
-            <img src="/images/card-img-default.jpg" alt="card-img" className="header-image w-full h-full bg-cover" />
-          </div>
-          <div className="card-content p-5 flex flex-col gap-2">
-            <h1 className="card-title font-bold">Name</h1>
-            <div className="status-wrapper flex items-center gap-2">
-              <span className="status bg-green-500 text-white px-2 py-1 rounded">Active</span>
-              <span className="status bg-blue-500 text-white px-2 py-1 rounded">Gender</span>
-              <span className="status bg-amber-500 text-white px-2 py-1 rounded">Species</span>
-            </div>
-            <Button>View All Episode</Button>
-          </div>
-        </div>
+        {datas.map((item, index) => (
+          <CardFragments key={index}>
+            <CardFragments.CardHeader images={item.image} />
+            <CardFragments.CardContent name={item.name} status={item.status} species={item.species} gender={item.gender}>
+              Click me
+            </CardFragments.CardContent>
+          </CardFragments>
+        ))}
       </div>
     </>
   );
--- src/components/fragments/CardFragment/index.jsx
+++ src/components/fragments/CardFragment/index.jsx
@@ -1,31 +1,36 @@
-import React from "react";
+import Button from "../../elements/Button";
 
-const CardFragments = ({ images, name, gender, species, status }) => {
+const CardFragments = ({ key, children }) => {
   return (
-    <div className="card h-auto border-2 border-gray-300 rounded-lg shadow-lg">
-      <CardHeader images={images} />
+    <div className="card  border-2 border-gray-300 rounded-lg shadow-lg" key={key}>
+      {children}
     </div>
   );
 };
 
 const CardHeader = ({ images }) => {
   return (
-    <div className="header-card w-full">
+    <div className="header-card w-full h-50">
       <img src={images} alt={images} className="header-image w-full h-full bg-cover" />
     </div>
   );
 };
 
-const cardContent = ({ name, gender, species, status = "Active" }) => {
-  <div className="card-content p-5 flex flex-col gap-2">
-    <h1 className="card-title font-bold">{name}</h1>
-    <div className="status-wrapper flex items-center gap-2">
-      <span className="status bg-green-500 text-white px-2 py-1 rounded">{status}</span>
-      <span className="gender bg-blue-500 text-white px-2 py-1 rounded">{gender}</span>
-      <span className="species bg-amber-500 text-white px-2 py-1 rounded">{species}</span>
+const CardContent = ({ name, gender, species, status = "Active", children }) => {
+  return (
+    <div className="card-content p-5 flex flex-col gap-2">
+      <h1 className="card-title font-bold">{name}</h1>
+      <div className="status-wrapper flex items-center gap-2">
+        <span className={`status ${status === "Dead" ? "bg-red-500" : status === "Alive" ? "bg-green-500" : "bg-gray-400"} text-white px-2 py-1 rounded`}>{status}</span>
+        <span className="gender bg-blue-500 text-white px-2 py-1 rounded">{gender}</span>
+        <span className="species bg-amber-500 text-white px-2 py-1 rounded">{species}</span>
+      </div>
+      <Button>{children}</Button>
     </div>
-    <Button>View All Episode</Button>
-  </div>;
+  );
 };
+
+CardFragments.CardHeader = CardHeader;
+CardFragments.CardContent = CardContent;
 
 export default CardFragments;
